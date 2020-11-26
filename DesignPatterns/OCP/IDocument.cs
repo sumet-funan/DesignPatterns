d69eeab5d0d64b07fe2e93e1eb7ef5f62bd91da9
sfunan <sfunan@celestica.com>
2020-11-26 17:17:27 +0700

    [OCP] Add IDocument

---
+++ DesignPatterns/OCP/IDocument.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP
+{
+    public interface IDocument
+    {
+        int TotalPages();
+        string ReadAllText(int pageIndex);
+    }
+}
