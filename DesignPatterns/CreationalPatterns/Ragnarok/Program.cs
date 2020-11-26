a22dbe153df1421bafc31a8a101076f0b703b9b0
sumet-funan <sumet.funan@outlook.com>
2020-11-26 23:21:32 +0700

    [Ragnarok] Add SimpleSlimeFactory

--- DesignPatterns/CreationalPatterns/Ragnarok/Program.cs
+++ DesignPatterns/CreationalPatterns/Ragnarok/Program.cs
@@ -11,59 +11,27 @@ namespace Ragnarok
         {
             Console.WriteLine("Welcome to Ragnarok!");
 
-            Slime mySlime = CreateNewSlime("Payon", "12:00");
+            SimpleSlimeFactory simpleSlimeFactory = new SimpleSlimeFactory();
+
+            Slime mySlime = simpleSlimeFactory.CreateNewSlime("Payon", "12:00");
             Console.WriteLine(JsonSerializer.Serialize(mySlime));
 
-            mySlime = CreateNewSlime("Payon", "18:00");
+            mySlime = simpleSlimeFactory.CreateNewSlime("Payon", "18:00");
             Console.WriteLine(JsonSerializer.Serialize(mySlime));
 
-            mySlime = CreateNewSlime("Payon", "");
+            mySlime = simpleSlimeFactory.CreateNewSlime("Payon", "");
             Console.WriteLine(JsonSerializer.Serialize(mySlime));
 
-            mySlime = CreateNewSlime("Desert", "12:00");
+            mySlime = simpleSlimeFactory.CreateNewSlime("Desert", "12:00");
             Console.WriteLine(JsonSerializer.Serialize(mySlime));
 
-            mySlime = CreateNewSlime("Desert", "19:00");
+            mySlime = simpleSlimeFactory.CreateNewSlime("Desert", "19:00");
             Console.WriteLine(JsonSerializer.Serialize(mySlime));
 
-            mySlime = CreateNewSlime("Desert", "");
+            mySlime = simpleSlimeFactory.CreateNewSlime("Desert", "");
             Console.WriteLine(JsonSerializer.Serialize(mySlime));
 
             Console.WriteLine("Bye bye!");
-        }
-
-        private static Slime CreateNewSlime(string mapName, string currentTime)
-        {
-            if (mapName == "Payon")
-            {
-                if (currentTime == "12:00")
-                {
-                    return new Poring("Poison");
-                }
-                else if (currentTime == "18:00")
-                {
-                    return new Angeling();
-                }
-                else
-                {
-                    return new Poporing();
-                }
-            }
-            else
-            {
-                if (currentTime == "12:00")
-                {
-                    return new Poring("Fire");
-                }
-                else if (currentTime == "19:00")
-                {
-                    return new Ghostring();
-                }
-                else
-                {
-                    return new Drops();
-                }
-            }
         }
     }
 }
