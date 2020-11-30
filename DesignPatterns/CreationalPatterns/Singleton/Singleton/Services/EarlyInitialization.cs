3725806839c317aab99ba9dc9d44113af255db96
sumet-funan <sumet.funan@outlook.com>
2020-11-30 21:57:25 +0700

    [Singleton] Initial Singleton

---
+++ DesignPatterns/CreationalPatterns/Singleton/Singleton/Services/EarlyInitialization.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton.Services
+{
+    public class EarlyInitialization
+    {
+        private static EarlyInitialization instance = new EarlyInitialization();
+
+        public static EarlyInitialization GetInstance()
+        {
+            return instance;
+        }
+
+        private EarlyInitialization() { }
+    }
+}
---
+++ DesignPatterns/CreationalPatterns/Singleton/Singleton/Services/LazyInitialization.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton.Services
+{
+    public class LazyInitialization
+    {
+        private static LazyInitialization instance;
+
+        private LazyInitialization() { }
+
+        public static LazyInitialization GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new LazyInitialization();
+            }
+            return instance;
+        }
+    }
+}
